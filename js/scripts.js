8cfaa35008bd608b61f46349278532c8e638415d
Jieun Kang and Joseph Wangemann <jieun@>
2020-01-22 16:33:36 -0800

    add function on recursion

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,23 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#inputNum").val());
-    if (input >= 0) {
-      var factorial = 1;
-      for (var i=input; i>0; i--) {
-        factorial *= i }
-    } else { 
-      alert("Please, enter a positive number");
-      location.reload();
-    }
-  $("#output").text(factorial);
+    var output = factorial(input);
+    $("#output").text(output);
   });
 });
+
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1)
+  }
+}
+// if (input >= 0) {
+//   var factorial = 1;
+//   for (var i=input; i>0; i--) {
+//     factorial *= i }
+// } else { 
+//   alert("Please, enter a positive number");
+//   location.reload();
+// }
